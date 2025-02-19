9dfe2442d2a4b4f2db000c30e5e82ca9e61cba38
mo-ri-regen <moricontb@gmail.com>
2025-02-19 22:50:53 +0900

    ステータスコード修正

--- lambda_function.py
+++ lambda_function.py
@@ -2,5 +2,5 @@ import json
 
 
 def lambda_handler(event, context):
-    return {"statusCode": 500, "body": json.dumps({"message": "Hello, Lambda!"})}
-    # return {"statusCode": 200, "body": json.dumps({"message": "Hello, Lambda!"})}
+    # return {"statusCode": 500, "body": json.dumps({"message": "Hello, Lambda!"})}
+    return {"statusCode": 200, "body": json.dumps({"message": "Hello, Lambda!"})}
